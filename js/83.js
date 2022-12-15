6572e5632b7894238b522935fb3d187a5c5258be
Evgeniy Salish <jeka5650962@gmail.com>
2022-12-15 16:18:47 +0300

    151222_16:18

---
+++ js/83.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    name: 'Bob',
+    legs: 4,
+    tails: 1,
+    friends: ['Tom', 'Kate', 'Felix']
+    // Only change code above this line
+};
