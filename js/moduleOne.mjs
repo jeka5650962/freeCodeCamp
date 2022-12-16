da82acbb78f1f6c17a8181795e7ff1b1cb118504
Evgeniy Salish <jeka5650962@gmail.com>
2022-12-16 17:25:37 +0300

    161222 1725

---
+++ js/moduleOne.mjs
@@ -0,0 +1,5 @@
+const sum = (a, b) => a + b
+const one = 1
+const two = 'two'
+
+export {sum, one, two}
---
+++ js/moduleTwo.mjs
@@ -0,0 +1,7 @@
+import {sum, one, two} from './moduleOne.mjs'
+
+const result = sum(10, 1)
+
+console.log(result)
+console.log(one)
+console.log(two)
