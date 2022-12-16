7415ff14fd1d4bf30ac76753feaa9969acf4a939
Evgeniy Salish <jeka5650962@gmail.com>
2022-12-16 09:52:40 +0300

    161222_09:52

---
+++ js/92.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Queen",
+        "title": "Rock",
+        "release_year": 1991,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ]
+    }
+];
