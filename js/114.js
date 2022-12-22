8d3da9e228b8d8d735773fa167bc10a4e68e00ec
Evgeniy Salish <jeka5650962@gmail.com>
2022-12-22 10:33:39 +0300

    221222 10 33

---
+++ js/114.js
@@ -0,0 +1,13 @@
+const s = [5, 7, 2];
+function editInPlace() {
+    // Only change code below this line
+
+    // Using s = [2, 5, 7] would be invalid
+    let removed = s.pop()
+    s.unshift(removed)
+    return s
+
+    // Only change code above this line
+}
+
+console.log(editInPlace())
