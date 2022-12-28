003c7fc9ccd43b18df5fdb48a41361099bdc1fa5
Evgeniy Salish <jeka5650962@gmail.com>
2022-12-28 19:07:52 +0300

    281222 19 07

--- codewars/5.js
+++ codewars/5.js
@@ -1,34 +1,5 @@
-var a1 = "A", a2 = "a", b1 = "B", b2 = "b", c1 = "C", c2 = "c", d1 = "D", d2 = "d", e1 = "E", e2 = "e", n1 = "N",
-    n2 = "n"
+const arr = {name: "dog", legs: 4, color: "white"}
 
-function Dad() {
-    //select some variable to combine "Dad"
-    return d1 + a2 + d2;
-}
+const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
 
-function Bee() {
-    //select some variable to combine "Bee"
-    return b1 + e2 + e2;
-}
-
-function banana() {
-    //select some variable to combine "banana"
-    return b2 + a2 + n2 + a2 + n2 + a2;
-}
-
-//answer some questions if you finished works above
-function answer1() {
-    //the answer should be "yes" or "no"
-    return "no";
-}
-
-function answer2() {
-    //the answer should be "yes" or "no"
-    return "no";
-}
-
-function answer3() {
-    //the answer should be "yes" or "no"
-    return "yes";
-}
-
+console.log(animal(arr))
