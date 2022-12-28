2b332562dec4dc5d4fb688317b5c245fafd41fef
Evgeniy Salish <jeka5650962@gmail.com>
2022-12-28 18:49:02 +0300

    281222 18 48

--- codewars/4.js
+++ codewars/4.js
@@ -1,39 +1,29 @@
-let v1 = 50,
-    v2 = 100,
-    v3 = 150,
-    v4 = 200,
-    v5 = 2,
-    v6 = 250;
-
-function equal1(){
-    let a = v1,
-        b = v1;
-    return a + b;
+function getLength(arr) {
+    //return length of arr
+    return arr.length
 }
 
-//Please refer to the example above to complete the following functions
-function equal2(){
-    let a = v4, //set number value to a
-        b = v2; //set number value to b
-    return a - b;
+function getFirst(arr) {
+    //return the first element of arr
+    return arr[0]
 }
 
-function equal3(){
-    let a = v1, //set number value to a
-        b = v5; //set number value to b
-    return a * b;
+function getLast(arr) {
+    //return the last element of arr
+    return arr[arr.length - 1]
 }
 
-function equal4(){
-    let a = v4, //set number value to a
-        b = v5; //set number value to b
-    return a / b;
+function pushElement(arr) {
+    var el = 1;
+    arr.push(el)
+    //push el to arr
+
+    return arr
 }
 
-function equal5(){
-    let a = v6, //set number value to a
-        b = v3; //set number value to b
-    return a % b;
-}
+function popElement(arr) {
+    //pop an element from arr
+    arr.pop()
 
-console.log(equal5())
+    return arr
+}
