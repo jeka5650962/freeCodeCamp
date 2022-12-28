e1b33ec51150da2de6ea7f4c55fa1d09da4e22c6
Evgeniy Salish <jeka5650962@gmail.com>
2022-12-28 18:11:40 +0300

    281222 18 11

--- codewars/2.js
+++ codewars/2.js
@@ -1,11 +1,11 @@
-function saleHotdogs(n) {
-    if (n < 5) {
-        return n * 100
-    } else if (n >= 5 && n < 10) {
-        return n * 95
-    } else {
-        return n * 90
-    }
-}
+let v1 = 50, v2 = 100, v3 = 150, v4 = 200, v5 = 2, v6 = 250;
 
-console.log(saleHotdogs(6))
+const equal1 = (a = v1, b = v1) => a + b
+
+const equal2 = (a = v4, b = v2) => a - b
+
+const equal3 = (a = v1, b = v5) => a * b
+
+const equal4 = (a = v4, b = v5) => a / b
+
+const equal5 = (a = v2, b = v4) => a % b
