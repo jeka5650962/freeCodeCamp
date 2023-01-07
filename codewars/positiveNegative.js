3a8f360be6f20356569c83608d787aca7c3e3914
Evgeniy Salish <jeka5650962@gmail.com>
2023-01-07 13:02:09 +0300

    070123 1302

--- codewars/positiveNegative.js
+++ codewars/positiveNegative.js
@@ -1,5 +1,14 @@
 const countPositivesSumNegatives = (input) => {
-
+    let resultArray = []
+    let temp = input.reduce((sum, item) => {
+        if (item < 0) {
+            return sum + item
+        }
+        return sum
+    }, 0)
+    console.log(temp)
 }
 
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+
+// [10, -65]
